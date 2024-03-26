54c2486346087926afb3bc3050956a2069f3a1d7
Wunna <mgwunna.mw@icloud.com>
2024-03-26 12:04:54 +0630

    fix: 🐛 properties name in svg of icon error

--- src/icons/check-icon.tsx
+++ src/icons/check-icon.tsx
@@ -4,13 +4,13 @@ const CheckIcon = () => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className="h-6 w-6"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="m4.5 12.75 6 6 9-13.5"
       />
     </svg>
