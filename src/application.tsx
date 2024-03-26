f345f160b1cd346afe96561c93bf5b36110b6d07
Wunna <mgwunna.mw@icloud.com>
2024-03-26 14:23:20 +0630

    refactor: 💡 application codes

--- src/application.tsx
+++ src/application.tsx
@@ -1,8 +1,10 @@
 import { useCallback, useEffect, useState } from "react";
+
 import AddTodo from "./components/add-todo";
 import TasksDoneSection from "./sections/tasks-done";
 import TasksTodoSection from "./sections/tasks-todo";
 
+// prettier-ignore
 /**
  * ### Application ###
  * @description This component can render todo application.
@@ -13,9 +15,9 @@ import TasksTodoSection from "./sections/tasks-todo";
  * @author Wunna
  */
 const App = () => {
-  const [keyword, setKeyword] = useState("");
-  const [todoList, setTodoList] = useState<string[]>([]);
-  const [doneList, setDoneList] = useState<string[]>([]);
+  const [ keyword  , setKeyword  ] = useState("");
+  const [ todoList , setTodoList ] = useState<string[]>([]);
+  const [ doneList , setDoneList ] = useState<string[]>([]);
 
   // Add task handler function
   const addTaskHandler = useCallback(() => {
@@ -31,7 +33,7 @@ const App = () => {
     (todoIndex: number) => {
       // clone todo list, get todo item and remove todo item from todo list
       const todoListClone = [...todoList];
-      const todoItem = todoListClone[todoIndex];
+      const todoItem      = todoListClone[todoIndex];
       todoListClone.splice(todoIndex, 1);
 
       // add todo item into done list and set removed todo list into todo list state
@@ -65,25 +67,25 @@ const App = () => {
 
   // update todo list, done list and keyword state into local storage
   useEffect(() => {
-    localStorage.setItem("todoList", JSON.stringify(todoList));
-    localStorage.setItem("doneList", JSON.stringify(doneList));
-    localStorage.setItem("keyword", keyword);
+    localStorage.setItem("todoList" , JSON.stringify(todoList));
+    localStorage.setItem("doneList" , JSON.stringify(doneList));
+    localStorage.setItem("keyword"  , keyword);
   }, [doneList, keyword, todoList]);
 
   return (
     <div className="flex h-screen w-screen items-center justify-center">
+      {/* Centered division */}
       <div className="flex h-[80vh] w-[70vw] flex-col overflow-hidden rounded-xl border border-solid border-gray-200 bg-gray-50 p-8 pb-0 shadow-md">
-        <AddTodo
-          keyword={keyword}
-          onKeywordChange={setKeyword}
-          onAddClick={addTaskHandler}
-        />
+        {/* Add todo input and button */}
+        <AddTodo keyword={keyword} onKeywordChange={setKeyword} onAddClick={addTaskHandler} />
+
+        {/* Divide line */}
         <div className="my-4 h-px w-full bg-gray-200" />
-        <TasksTodoSection
-          todoList={todoList}
-          checkTodoHandler={checkTodoHandler}
-          deleteTodoHandler={deleteTodoHandler}
-        />
+
+        {/* Tasks todo list section */}
+        <TasksTodoSection todoList={todoList} checkTodoHandler={checkTodoHandler} deleteTodoHandler={deleteTodoHandler} />
+
+        {/* Tasks done list section */}
         <TasksDoneSection doneList={doneList} />
       </div>
     </div>
