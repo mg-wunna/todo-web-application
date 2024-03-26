59048379166c735d9a01662f5d063ebadfa58b8c
Wunna <mgwunna.mw@icloud.com>
2024-03-26 14:29:59 +0630

    refactor: 💡 icons codes

--- src/icons/check-icon.tsx
+++ src/icons/check-icon.tsx
@@ -1,3 +1,4 @@
+// prettier-ignore
 /**
  * ### Check Icon ###
  * @description This icon can render check svg.
@@ -9,19 +10,8 @@
  */
 const CheckIcon = () => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      className="h-6 w-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="m4.5 12.75 6 6 9-13.5"
-      />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6" >
+      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
     </svg>
   );
 };
--- src/icons/plus-icon.tsx
+++ src/icons/plus-icon.tsx
@@ -1,3 +1,4 @@
+// prettier-ignore
 /**
  * ### Plus Icon ###
  * @description This icon can render plus svg.
@@ -9,19 +10,8 @@
  */
 const PlusIcon = () => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      className="h-6 w-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M12 4.5v15m7.5-7.5h-15"
-      />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6" >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
     </svg>
   );
 };
