60b24400b6eb5acce7d94ecbb247bc3b82d8da1a
Wunna <mgwunna.mw@icloud.com>
2024-03-26 12:01:47 +0630

    feat: 🎸 add check, plus and trash icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import CheckIcon from "./icons/check-icon";
+import PlusIcon from "./icons/plus-icon";
+import TrashIcon from "./icons/trash-icon";
+
 const App = () => {
-  return <div className="text-3xl font-bold underline">App</div>;
+  return (
+    <div>
+      <CheckIcon />
+      <PlusIcon />
+      <TrashIcon />
+    </div>
+  );
 };
 
 export default App;
