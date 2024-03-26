52f2419cc511ad8815df709a6c97badce11f60d0
Wunna <mgwunna.mw@icloud.com>
2024-03-26 14:33:45 +0630

    refactor: 💡 add todo and todo item components codes

--- src/components/add-todo.tsx
+++ src/components/add-todo.tsx
@@ -11,6 +11,7 @@ export type AddTodoProps = {
   onAddClick: () => void;
 };
 
+// prettier-ignore
 /**
  * ### Add todo component ###
  * @description This component can render add todo.
@@ -36,17 +37,14 @@ const AddTodo = ({ keyword, onKeywordChange, onAddClick }: AddTodoProps) => {
 
   return (
     <div className="flex h-10 items-center justify-between gap-4">
-      <input
-        type="text"
-        placeholder="Add a new task"
-        className="flex h-full flex-1 items-center rounded-md border border-solid border-gray-200 bg-gray-50 px-4"
-        value={keyword}
-        onChange={changeKeywordHandler}
-      />
-      <div
-        className="flex aspect-square h-full items-center justify-center rounded-md bg-blue-500 text-white"
-        onClick={onAddClick}
-      >
+      {/* Input box */}
+      <input className="flex h-full flex-1 items-center rounded-md border border-solid border-gray-200 bg-gray-50 px-4"
+             type="text" placeholder="Add a new task"
+             value={keyword} onChange={changeKeywordHandler} />
+
+      {/* Add button */}
+      <div className="flex aspect-square h-full items-center justify-center rounded-md bg-blue-500 text-white"
+           onClick={onAddClick} >
         <PlusIcon />
       </div>
     </div>
--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -14,6 +14,7 @@ export type TodoItemProps = {
   onDelete?: () => void;
 };
 
+// prettier-ignore
 /**
  * ### Todo item component ###
  * @description This component can render todo item.
@@ -32,19 +33,19 @@ export type TodoItemProps = {
 const TodoItem = memo(({ text, isDone, onCheck, onDelete }: TodoItemProps) => {
   return (
     <div className="flex items-center gap-2 rounded-md border-b border-solid border-gray-200 p-2 py-3">
+      {/* Text label */}
       <p className={`flex-1 ${isDone ? "line-through" : ""}`}>{text}</p>
       {!isDone && (
         <div className="flex items-center gap-2">
-          <div
-            className="flex aspect-square h-full items-center justify-center rounded-md bg-blue-500 p-1 text-white"
-            onClick={onCheck}
-          >
+          {/* Check icon button */}
+          <div className="flex aspect-square h-full items-center justify-center rounded-md bg-blue-500 p-1 text-white"
+               onClick={onCheck} >
             <CheckIcon />
           </div>
-          <div
-            className="flex aspect-square h-full items-center justify-center rounded-md bg-blue-500 p-1 text-white"
-            onClick={onDelete}
-          >
+
+          {/* Delete icon button */}
+          <div className="flex aspect-square h-full items-center justify-center rounded-md bg-blue-500 p-1 text-white"
+               onClick={onDelete} >
             <TrashIcon />
           </div>
         </div>
